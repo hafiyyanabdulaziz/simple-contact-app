f0e1f47697e3bbb8a2ae009063a267d9f546ae50
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2024-05-08 20:13:56 +0700

    feature(header): create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className=''>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button className='btn'>Click</button>
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='navbar bg-base-300'>
+      <div className='navbar-start' />
+      <div className='navbar-center '>
+        <h1 className='text-xl font-bold'>Hafi Contacts</h1>
+      </div>
+      <div className='navbar-end' />
+    </div>
+  );
+};
+
+export default Header;
