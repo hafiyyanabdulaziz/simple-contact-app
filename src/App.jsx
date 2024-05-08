83213c5746370dffbb420b3e32368f87f5d06be1
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2024-05-08 19:03:40 +0700

    install: tailwindcss & daisyui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=''>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <button className='btn'>Click</button>
+    </div>
+  );
+};
+
+export default App;
