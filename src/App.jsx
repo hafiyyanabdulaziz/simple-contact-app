76835482f0b9737c0347f903350ddbcde7c8f423
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2024-05-08 20:16:06 +0700

    feature(footer): create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className=''>
       <Header />
+      <main className='min-h-screen'></main>
+      <Footer />
     </div>
   );
 };
