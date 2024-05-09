a7010d6610e390bc9899dc558440b51fb0ffb803
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2024-05-09 22:35:10 +0700

    refactor(search): add info in searchbar

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,23 +1,16 @@
 import React from "react";
+import SearchIcon from "../../assets/icons/SearchIcon";
 
 const Search = () => {
   return (
-    <label className='input input-bordered flex items-center gap-2'>
-      <input type='text' className='grow' placeholder='Search Name or Number' />
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-        strokeWidth={1.5}
-        stroke='currentColor'
-        className='w-6 h-6'>
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-        />
-      </svg>
-    </label>
+    <div
+      className='tooltip'
+      data-tip='This feature is only available for premium members.'>
+      <label className='input input-bordered flex items-center gap-2'>
+        <input type='text' className='grow' placeholder='Search' />
+        <SearchIcon />
+      </label>
+    </div>
   );
 };
 
